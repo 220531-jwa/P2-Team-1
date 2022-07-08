3db1b22087356b88f1f57b0a265f47ec23df2f00
k-corbett <katherine.a.corbett@gmail.com>
2022-07-08 16:53:19 -0400

    adding login testing

--- src/test/java/runner/AchieveTestSuite.java
+++ src/test/java/runner/AchieveTestSuite.java
@@ -1,10 +1,17 @@
 package runner;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.Suite;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import pages.BuyerPage;
+import pages.LoginPage;
+import pages.createAccountPage;
+
+import java.io.File;
 
 @Suite
 @Tag("CucumberTests")
@@ -12,4 +19,22 @@ public class AchieveTestSuite {
 	
 	public static WebDriver driver;
 	public static BuyerPage bp;
+
+	public static createAccountPage cap;
+	public static LoginPage lp;
+
+	@BeforeAll
+	public static void setup(){
+		File chrome = new File("src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
+		driver = new ChromeDriver();
+		bp = new BuyerPage(driver);
+		cap = new createAccountPage(driver);
+		lp = new LoginPage(driver);
+	}
+
+	@AfterAll
+	public static void teardown(){
+		driver.quit();
+	}
 }
